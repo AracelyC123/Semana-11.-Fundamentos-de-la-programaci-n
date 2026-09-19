cd07498f07b902ce20c1ab8440777743d0c6c607
Aracely Coyago <ma.coyagot@uea.edu.ec>
2026-09-19 09:54:52 -0600

    Add files via upload

---
+++ Tarea.py
@@ -0,0 +1,11 @@
+def calcular_promedio (nota1,nota2,nota3):
+    promedio = (nota1 + nota2 + nota3) / 3
+    return promedio 
+
+
+nota1= float (input("Ingrese la primera nota: "))
+nota2= float (input("Ingrese la segunda nota: "))
+nota3= float (input("Ingrese la tercera nota: "))
+
+resultado = calcular_promedio(nota1, nota2, nota3)
+print("El promedio de las notas es:", resultado)
